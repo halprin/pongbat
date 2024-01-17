561634557fbe4202d6d76f29e63b8916c69b698a
halprin <halprin@users.noreply.github.com>
2024-01-16 22:56:06 -0700

    Have bomb ball use the new form of converting from another ball

--- app/main_game/ball/bomb_ball.rb
+++ app/main_game/ball/bomb_ball.rb
@@ -1,54 +1,11 @@
 class BombBall < Ball
 
-  @@width = 20
-  @@height = 20
-
-  def initialize(args, starting_x, starting_y)
-    super(args, '/sprites/ball/bomb.png', starting_x, starting_y, @@width, @@height)
-
-    @dx = (rand(5) + 1) * [1, -1].sample
-    @dy = (rand(5) + 1) * [1, -1].sample
-
+  def initialize(args, other_ball)
+    super(args, 'bomb.png', other_ball.x, other_ball.y, other_ball.dx, other_ball.dy)
   end
 
   # Action methods
 
-  def bounce_sides
-    @dx *= -1
-  end
-
-  def bounce_top_bottom
-    @dy *= -1
-  end
-
-  def left_claim
-    @path = '/sprites/ball/blue.png'
-  end
-
-  def right_claim
-    @path = '/sprites/ball/red.png'
-  end
-
-  def speed_up
-    # add 1 or subtract 1, depending on whether we already have a positive or negative dx, dy
-    @dx += @dx / @dx.abs
-    @dy += @dy / @dy.abs
-  end
-
-  def speed_down
-    # add 1 or subtract 1, depending on whether we already have a positive or negative dx, dy
-    @dx -= @dx / @dx.abs
-    @dy -= @dy / @dy.abs
-  end
-
   # Calculation methods
 
-  def calculate(args)
-    move_ball
-  end
-
-  def move_ball
-    @x += @dx
-    @y += @dy
-  end
 end
